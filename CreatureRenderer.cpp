30acaf67f90b6c7da593496ef6691c27d1362427
rh101 <rh101@users.noreply.github.com>
2018-05-28 16:10:13 +1000

    Texture can become invalid
    The texture being used by the CreatureRenderer can be freed and become invalid if it is not explicitly set to be retained.  This change stops it from becoming invalid until it is no longer used by any object.

--- CreatureRenderer.cpp
+++ CreatureRenderer.cpp
@@ -50,7 +50,8 @@ namespace CreatureRenderer {
                        cocos2d::Texture2D * texture_in)
     : manager(manager_in), texture(texture_in), debug_draw(false)
     {
-        setGLProgram(cocos2d::ShaderCache::getInstance()->getGLProgram(cocos2d::GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR));
+        setGLProgram(cocos2d::GLProgramCache::getInstance()->getGLProgram(cocos2d::GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR));
+        setTexture(texture_in);
         scheduleUpdate();
     }
     
@@ -156,6 +157,15 @@ namespace CreatureRenderer {
 		metadata = std::make_unique<CreatureModule::CreatureMetaData>(str_stream.str());
 		meta_indices.resize(manager->GetCreature()->GetTotalNumIndices());
 	}
+    
+    void Renderer::setTexture(cocos2d::Texture2D* texture_in)
+    {
+        if (texture != texture_in) {
+            CC_SAFE_RELEASE(texture);
+            CC_SAFE_RETAIN(texture_in);
+            texture = texture_in;
+        }
+    }
     
     void
     Renderer::SetDebugDraw(bool flag_in)
